163b9ca3b7de933a1c3fd15d90759564902bb0dc
Sam Schantz <samvschantz@gmail.com>
2018-03-19 22:59:44 +0000

    Main function works with hardcoded values

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,6 @@
 var request = require('request');
-var secret = require('./secrets')
+var secret = require('./secrets');
+var fs = require('fs');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
@@ -13,11 +14,24 @@ function getRepoContributors(repoOwner, repoName, cb) {
   };
 
   request(options, function(err, res, body) {
-    cb(err, body);
+    cb(err, JSON.parse(body));
   });
+}
+
+function downloadImageByURL(url, filePath) {
+  request.get(url + filePath)
+    .on('error', function (err) {
+      throw err;
+    })
+    .on('response', function (response) {
+      console.log('Response Status Code: ', response.statusCode + '\n' + 'Downloading...');
+    })
+    .pipe(fs.createWriteStream('./githubAvatars'))
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
 });
+
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
