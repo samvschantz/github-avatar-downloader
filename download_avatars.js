adb8bf752c09816479288e02bacdf3b3e76c9f7d
Sam Schantz <samvschantz@gmail.com>
2018-03-20 01:39:25 +0000

    Successful gitignore for avatar files

--- download_avatars.js
+++ download_avatars.js
@@ -1,6 +1,7 @@
 var owner = process.argv[2]
 var repo = process.argv[3]
 
+require('dotenv').config()
 var request = require('request');
 var secret = require('./secrets');
 var fs = require('fs');
@@ -12,14 +13,14 @@ function getRepoContributors(repoOwner, repoName, cb) {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
       'User-Agent': 'request',
-      'Authorization': secret.GITHUB_TOKEN
+      'Authorization': process.env.DB_KEY
     }
   };
 
   request(options, function(err, res, body) {
     var parsedJSON = JSON.parse(body)
     cb(err, parsedJSON);
-    parsedJSON.forEach(function(element){
+    parsedJSON.forEach(function(element){ //parses each JSON obj as it comes through
       var filePath = element['login']
       var url = element['avatar_url']
       var position = parsedJSON.indexOf(element) + 1
@@ -27,7 +28,7 @@ function getRepoContributors(repoOwner, repoName, cb) {
     })
   });
 }
-
+  //position not required and may be confusing to user due to asynchrony
 function downloadImageByURL(url, filePath, position) {
   request.get(url + filePath)
     .on('error', function (err) {
@@ -36,10 +37,11 @@ function downloadImageByURL(url, filePath, position) {
     .on('response', function (response) {
       console.log('Downloading image #' + position);
     })
-    .pipe(fs.createWriteStream('githubAvatars/' + filePath))
+    .pipe(fs.createWriteStream('avatars/' + filePath))
 }
 
 getRepoContributors(owner, repo, function(err, result) {
+  //tried to figure out how to end program to avoid showing stack trace to user
   if (!owner){
     throw new Error('ERROR: Please enter an owner and a repo in the format: node download_avatars.js <owner> <repo>')
   } else if (!repo){
